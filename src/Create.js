e0719b20efa3a22eef90a6fefb6ecc34fa3a4c4b
EslamFoda <eslaamfodaa@gmail.com>
2023-01-28 16:18:54 +0200

    update img

--- src/Create.js
+++ src/Create.js
@@ -2,15 +2,16 @@ import Nav from "./Nav";
 import "./Create.css";
 import { useHistory } from "react-router";
 import { useRef, useState } from "react";
-import {  useMutation } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import { CREATE_POST_MUTATION, ALL_POSTS } from "./queries";
 
 const Create = () => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const [loadingImg, setLoadingImg] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [picture, setPicture] = useState("");
+  const [uploadedImg, setUploadedImg] = useState();
   const backgroundInput = useRef(null);
   const type = ["image/jpeg", "image/png"];
   const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
@@ -22,17 +23,16 @@ const Create = () => {
       variables: {
         createPostInput: {
           title,
-          picture,
+          picture: uploadedImg,
           content,
         },
       },
     });
     history.push("/");
-  
   };
 
   const changeHandler = async (e) => {
-    // setError(false);
+    setLoadingImg(true);
     const selected = e.target.files[0];
     const reader = new FileReader();
     reader.addEventListener("load", () => {});
@@ -42,23 +42,20 @@ const Create = () => {
     const formData = new FormData();
     formData.append("file", e.target.files[0]);
     formData.append("upload_preset", "my-upload");
-    if (selected && type.includes(selected.type)) {
-      const data = await fetch(
-        "https://api.cloudinary.com/v1_1/dxrdyke2n/image/upload",
-        {
-          method: "POST",
-          body: formData,
-        }
-      ).then((r) => r.json());
-      console.log(data.secure_url)
-    //   setStoredImgs((oldArray) => [...oldArray, data.secure_url]);
-    //   itemIndex || itemIndex === 0
-    //     ? (comps[compIndex].compData.items[itemIndex].pic = data.secure_url)
-    //     : (comps[compIndex].compData.pic = data.secure_url);
-
-    //   setComps([...comps]);
-    } else {
-    //   setError(true);
+    try {
+      if (selected && type.includes(selected.type)) {
+        const data = await fetch(
+          "https://api.cloudinary.com/v1_1/dxrdyke2n/image/upload",
+          {
+            method: "POST",
+            body: formData,
+          }
+        ).then((r) => r.json());
+        setUploadedImg(data.secure_url);
+        setLoadingImg(false);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -87,24 +84,58 @@ const Create = () => {
             onChange={(e) => setContent(e.target.value)}
           ></textarea>
           <label>picture :</label>
-          <div style={{border:"1px solid black"}}>
-            <div
-              onClick={onButtonClick}
-              className="h-full cursor-pointer group w-full rounded-md border border-solid flex items-center flex-col justify-center border-gray-500"
-            > 
-            add img
-             
-              <input
-                type="file"
-                id="file"
-                style={{ display: "none" }}
-                ref={backgroundInput}
-                onChange={changeHandler}
-              />
-              <span className="text-[#868c96] text-xl group-hover:text-white">
-                upload image
-              </span>
-            </div>
+          <div
+            style={{
+              border: uploadedImg ? "1px solid #d9d9d9" : "1px dashed  #d9d9d9",
+              background: uploadedImg ? "white" : "rgba(0,0,0,.02)",
+            }}
+            className="image-container"
+          >
+            {uploadedImg ? (
+              <div className="overlay">
+                <div
+                  className="uploaded-img"
+                  style={{ backgroundImage: `url(${uploadedImg})` }}
+                ></div>
+                <div className="delete-img-container">
+                  <span
+                    onClick={() => {
+                      setUploadedImg("");
+                    }}
+                  >
+                    d
+                  </span>
+                </div>
+              </div>
+            ) : (
+              <>
+                {loadingImg ? (
+                  <div className="loading-upload-container">
+                    <span>Uploading...</span>
+                    <div class="lds-ring">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="image-btn" onClick={onButtonClick}>
+                    <input
+                      type="file"
+                      id="file"
+                      style={{ display: "none" }}
+                      ref={backgroundInput}
+                      onChange={changeHandler}
+                    />
+                    <div className="icon-container">
+                      <span className="plus-icon">+</span>
+                      <span className="upload-text">Upload</span>
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
           </div>
           {loading && (
             <button disabled className="btn-disabled" type="submit">
